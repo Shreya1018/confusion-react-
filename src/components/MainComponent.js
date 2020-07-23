e0e18d3c1141c89bc3b1584bc442c6040b978975
Chinoori Shreya Reddy <shreyareddychinoori@gmail.com>
2020-07-23 18:55:47 +0530

    Week 2 Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent' ;
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES }  from '../shared/dishes';
@@ -35,7 +36,7 @@ class Main extends Component {
             comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
     };
-    
+
     return (
       <div >
         <Header />
@@ -44,6 +45,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId}/>
           <Route exact path='/contactus' component={Contact} />
+          <Route path='/aboutus' component={()=> <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
       
